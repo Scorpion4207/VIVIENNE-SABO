9764cba71e3310195ead34ab2e6ae54473ab7f00
Scorpion4207 <telitsin.andrei2015@yandex.ru>
2024-02-25 22:44:15 +0700

    fixed test-progress js modal

--- js/modal_loading.js
+++ js/modal_loading.js
@@ -1,55 +1,53 @@
 (() => {
-  
-    // ==================================Модалка с загрузкой============================
 
-    const uploadBtn = document.querySelector('.upload-btn');
-    const fileInput = document.getElementById('file-input');
-    const progressModal = document.querySelector('.progress-modal');
-    const modal = document.querySelector('.modal');
-    const progressBar = document.querySelector('.progress');
-    
-    uploadBtn.addEventListener('click', () => {
-      fileInput.click(); 
-    });
-    
-    fileInput.addEventListener('change', () => {
-      const file = fileInput.files[0];
-      modal.classList.add('hidden');
-      let progressValue = document.getElementById('progressValue');
-      let i = 0;
-      let interval = setInterval(function() {
-        if (i >= 100) {
+  // ==================================Модалка с загрузкой============================
+
+  const uploadBtn = document.querySelector('.upload-btn');
+  const fileInput = document.getElementById('file-input');
+  const progressModal = document.querySelector('.progress-modal');
+  const progressText = document.querySelector('.progress-text');
+  const modal = document.querySelector('.modal');
+  const progressBar = document.querySelector('.progress');
+
+  uploadBtn.addEventListener('click', () => {
+    fileInput.click();
+  });
+
+  fileInput.addEventListener('change', () => {
+    const file = fileInput.files[0];
+    modal.classList.add('hidden');
+
+    if (file) {
+      showProgressModal();
+      let progress = 0;
+      const interval = setInterval(() => {
+
+        progress += 1;
+        progressText.textContent = `${progress}%`;
+
+        progressModal.classList.remove('hidden');
+        progress += 1;
+        progressBar.style.width = `${progress}%`;
+
+
+
+        if (progress === 100) {
           clearInterval(interval);
-          modal.style.display = 'none';
-        } else {
-          i++;
-          progressValue.textContent = i;
-        }
-      }, 0); 
-      if (file) {
-        showProgressModal();
-        
-        let progress = 0;
-        const interval = setInterval(() => {
-            progressModal.classList.remove('hidden');
-          progress += 5;
-          progressBar.style.width = `${progress}%`;
-          
-          if (progress === 100) {
-            clearInterval(interval);
-            progressModal.classList.add('hidden');
+          progressModal.classList.add('hidden');
 
-            setTimeout(() => {
-              window.location.href = "photo_ready.html";
-            }, 0); 
-          }
-        }, 150);
-      }
-    });
-    
-    
-    function showProgressModal() {
-      progressModal.classList.remove('hidden');
+
+
+          setTimeout(() => {
+            window.location.href = "photo_ready.html";
+          }, 0);
+        }
+      }, 70);
     }
-    
+  });
+
+
+  function showProgressModal() {
+    progressModal.classList.remove('hidden');
+  }
+
 })()
